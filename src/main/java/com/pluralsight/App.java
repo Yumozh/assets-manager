bf78856e6d3f1832420428f9e1767d4c0699a0b4
Yumozh <juliiamozhchil@gmail.com>
2026-05-12 14:13:14 -0500

    Calculate value of the vehicle and house

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class App {
+    public static void main(String[] args) {
+        ArrayList<Asset> assetsList = new ArrayList<>();
+
+    }
 }
--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,8 +1,59 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+
 public class Vehicle extends Asset{
+    private String makeModel;
+    private int year;
+    private int odometer;
 
     public Vehicle(String description, String dateAcquired, double originalCost) {
         super(description, dateAcquired, originalCost);
+    }
+
+    public String getMakeModel() {
+        return makeModel;
+    }
+
+    public void setMakeModel(String makeModel) {
+        this.makeModel = makeModel;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+    @Override
+    public double getValue() {
+        int yearNow = LocalDate.now().getYear();
+        int age = yearNow - year;
+        double reducedValue;
+
+        if(age <= 3) {
+            reducedValue = getValue() * Math.pow(0.97, age);
+        } else if (age <= 6) {
+            reducedValue = getValue() * Math.pow(0.94, age);
+        } else if (age <= 10) {
+            reducedValue = getValue() * Math.pow(0.92, age);
+        }else{
+            reducedValue = 1000;
+        }
+        if(odometer > 100_000){
+            if(!(makeModel.equalsIgnoreCase("Honda") || makeModel.equalsIgnoreCase("Toyota"))){
+                reducedValue = reducedValue * 0.75;
+            }
+        }
+        return reducedValue;
     }
 }
